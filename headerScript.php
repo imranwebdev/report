fd815114db9683e1f737650eca440a11858d1643
md imran <119500104+Tahidulislam5@users.noreply.github.com>
2022-12-01 00:11:06 +0600

    Update headerScript.php

--- headerScript.php
+++ headerScript.php
@@ -1,8 +1,8 @@
 <link rel="stylesheet" href="style.css" />
-<!-- <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css"rel="stylesheet"/> -->
-<link rel="stylesheet" href="bootstrap-5.2.3/dist/css/bootstrap.min.css" />
-<!-- <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js"></script> -->
-<script src="bootstrap-5.2.3/dist/js/bootstrap.bundle.min.js"></script>
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css"rel="stylesheet"/>
+<!-- <link rel="stylesheet" href="bootstrap-5.2.3/dist/css/bootstrap.min.css" /> -->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js"></script>
+<!-- <script src="bootstrap-5.2.3/dist/js/bootstrap.bundle.min.js"></script> -->
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css">
 <!-- script header css,bootstrap etc -->
 <script src="https://code.jquery.com/jquery-3.6.1.min.js"></script>
